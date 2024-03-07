964631a218e6cf59a129d2a9631e5ee28e605a03
ryujinzz <kdo2240k@naver.com>
2024-03-07 11:52:23 +0900

    [Chore] 배포 테스트용 Pr

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import './index.css';
 import React from 'react';
-import App from './app/App.tsx';
-import theme from './styles/theme.ts';
+import App from './app/App';
+import theme from './styles/theme';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles.ts';
+import GlobalStyles from './styles/GlobalStyles';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
       <GlobalStyles />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   </ThemeProvider>
 );
